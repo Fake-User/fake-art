be9bc2233b0bcd1423831de2907efb3857acb279
Fake-User <russellboswell@protonmail.com>
2022-06-13 15:18:28 -0700

    fixr

--- static/components/comp-landing.js
+++ static/components/comp-landing.js
@@ -18,12 +18,29 @@ templateCompLanding.innerHTML = `
                     height: 100%;
                     object-fit: cover;
                 }
+            #landing-text-grid{
+                width: 100%;
+                height: 100%;
+                display: grid;
+                grid-template: auto calc(50vh - calc(1.5 * calc(4vh + 4vw))) / 1fr;
+            }
+                #landing-text{
+                    grid-area: 1 / 1;
+                    mix-blend-mode: difference;
+                    font-family: too-many-cowboys;
+                    font-size: calc(4vh + 7vw);
+                    text-align: center;
+                    line-height: calc(4.1vh + 7vw);
+                }
     </style>
 
     <div class="landing">
         <div class="landing-video-wrapper"><!--fix for safari video position: fixed; bug-->
             <video id="landing-video" autoplay muted loop playsinline type="video/mp4"></video>
         </div>
+    </div>
+    <div id="landing-text-grid">
+        <div id="landing-text"></div>
     </div>
 `;
 
@@ -36,6 +53,7 @@ class CompLanding extends HTMLElement{
         /* Style */
         this.shadowRoot.getElementById('landing-video').src = this.getAttribute("src");
         this.shadowRoot.getElementById('landing-video').poster = this.getAttribute("poster");
+        this.shadowRoot.getElementById('landing-text').innerHTML = this.getAttribute("text");
     };
 };
 
