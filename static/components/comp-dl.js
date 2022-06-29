6a9b14a1a948f3c11f66745d2841e94ccd362f08
Fake-User <russellboswell@protonmail.com>
2022-06-29 00:33:47 -0700

    cunt

---
+++ static/components/comp-dl.js
@@ -0,0 +1,73 @@
+const templateDL = document.createElement("template");
+templateDL.innerHTML = `
+    <style>
+    a{
+        text-decoration: none;
+    }
+    #project{
+        width: 100%;
+        aspect-ratio: 1 / 1;
+        filter: drop-shadow(0 0 1vh #000);
+        border-radius: min(2vh, 1vw);
+        background: #0008;
+        background-size: cover;
+        display: block;
+    }
+        #project-text-wrapper{
+            width: 100%;
+            height: 100%;
+            display: grid;
+            grid-template-rows: 1fr 30% 1fr;
+            align-items: center;
+            filter: opacity(0%);
+            transition-duration: 1s;
+        }
+        #project-text-wrapper:hover{
+            filter: opacity(100%);
+            transition-duration: 0s;
+        }
+
+            #project-text{
+                grid-area: 2 / 1;
+                padding: 10% 0;
+                background: #000a;
+                letter-spacing: 50%;
+                text-align: center;
+                line-height: calc(0.5vh + 1.5vw);
+            }
+
+                #client{
+                    font-size: calc(1.5vh + 2vw);
+                    color: #fff;
+                }
+                #title{
+                    font-size: calc(0.5vh + 1vw);
+                    color: #fffa;
+                }
+    </style>
+    <a id="project" href="" target="_blank">
+        <div id="project-text-wrapper">
+            <div id="project-text">
+                <span id="client"></span><br><span id="title"></span>
+            </div>
+        </div>
+    </a>
+
+`;
+
+class CompDL extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(templateDL.content.cloneNode(true));
+
+        /* Style */
+        this.shadowRoot.getElementById("project").style.backgroundImage = "url('" + this.getAttribute("img") + "')";
+        this.shadowRoot.getElementById("client").innerHTML = this.getAttribute("client");
+        this.shadowRoot.getElementById("title").innerHTML = this.getAttribute("title");
+
+        this.shadowRoot.getElementById("project").href = this.getAttribute("link");
+    }
+};
+
+window.customElements.define("comp-dl", CompDL);
