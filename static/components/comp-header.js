5a19764e8d3c2e9080f36d661ebb4f93b65f0a31
Fake-User <russellboswell@protonmail.com>
2022-10-20 10:27:55 -0700

    discord

--- static/components/comp-header.js
+++ static/components/comp-header.js
@@ -13,7 +13,7 @@ templateCompHeader.innerHTML = `
             height: 9vh;
             background-color: #000e;
             display: grid;
-            grid-template-columns: 0 auto 1fr auto auto auto auto 0;
+            grid-template-columns: 0 auto 1fr auto auto auto auto auto 0;
             gap: 2.5vh;
             z-index: 9;
         }
@@ -54,6 +54,9 @@ templateCompHeader.innerHTML = `
         </a>
         <a href="https://github.com/Fake-User" target="_blank">
             <img class="icon highlight" src="/static/content/svg/svg-github.svg">
+        </a>
+        <a href="https://discord.gg/QtbRGDAxSk" target="_blank">
+            <img class="icon highlight" src="/static/content/svg/svg-discord.svg">
         </a>
         <a href="mailto:russellboswell@fakeart.online" target="_blank">
             <div class="button"><div>&nbsp&nbspWHATS&nbspUP&nbsp&nbsp</div></div>
